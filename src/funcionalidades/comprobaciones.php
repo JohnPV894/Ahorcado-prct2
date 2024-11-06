a3a8149b74839803f9092c3236657d9a0bf431aa
John Santiago Arenales Fernandez <johnsaf@aulaestudio.es>
2024-11-06 20:26:24 +0100

    1.0.2 verificar .data

--- src/funcionalidades/comprobaciones.php
+++ src/funcionalidades/comprobaciones.php
@@ -1,4 +1,15 @@
 <?php
-      $input=$_GET["input1"];
-      header("Location: ../interfaz/index.html");
-      echo $input;
+$servername = "localhost";
+$database = "";
+$username = "root";
+$password = "password";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+mysqli_close($conn);
+?>
+https://www.hostinger.es/tutoriales/conectar-php-mysql
--- src/funcionalidades/main.js
+++ src/funcionalidades/main.js
@@ -6,7 +6,8 @@
 
 //Variables del juego
 let diccionario=["hola","adios","computador","movil","mongodb","mysql"];
-let tiempo=0;
+let palabraOculta=diccionario[0];
+let segundos=60;
 let puntuacion=0;
 let intentosRestantes=5;
 
@@ -14,32 +15,78 @@ let intentosRestantes=5;
 let inputLetra=undefined;
 
 //#####################################################
+function actualizarTemporizador(Tiempo,contenedorHtml) {
+      $(contenedorHtml).append(Tiempo);
+      setTimeout(() => {
+            Tiempo--;
+            $(contenedorHtml).empty();
+            $(contenedorHtml).append(Tiempo);
+      }, 1000);
+      
 
+}
 function incluyeLetra(palabra,letra) {
 
       for (let contador = 0; contador < palabra.length; contador++) {
             if (palabra[contador]==letra) {
-                  return contador;
+                  //console.log("incluye"+contador);
+                  
+                  return contador+1;//Por alguna extraña razon contador necesita un +1 para funcionar correctamente
             }
 
       }
       return false;
 }
+function agregarContenedorPalabra(palabra,contenedorHtml) { 
 
+      for (let indice = 1; indice < palabra.length+1; indice++) {//n°2 extraño pero aqui igual
+            
+            $(contenedorHtml).append(`<p class="letra${indice}">${indice}</p>`);  
+            $(contenedorHtml).data("encontrado", false);  
+      }
+}
+function partidaFinalizada(palabra) {
+      for (let indice = 1; indice <= palabra.length; indice++) {
+            
+            if ($(`.lista${indice}`).data("encontrado")==true || intentosRestantes===0) {
+                  return true;
+            }
+
+      }
+      return false;
+}
 //JQUERY
 $(document).ready(async function () {
-      $(".palabraAdivinar").append(diccionario[0]);
+
+            $(".tiempo").append(segundos);
+      const temporizador=setInterval(() => { // setInterval() ejecuta un fragmento de código de forma reiterada, con un retardo de tiempo fijo entre cada llamada. 
+            segundos--;
+            if (partidaFinalizada(palabraOculta)) {
+                clearInterval(temporizador);
+            }
+            $(".tiempo").empty();
+            $(".tiempo").append(segundos);
+      }, 1000);
+
+
+      agregarContenedorPalabra(palabraOculta,".cadaLetra");
+      $(".palabraAdivinar").append(palabraOculta);
 
       $(".enviar").click(function () { 
-            //$("body").css("background-color", "red");
-            if (incluyeLetra(diccionario[0],$(".letraEntrada").val())==false) {
-                  
+
+            if (incluyeLetra(palabraOculta,$(".letraEntrada").val())==false) {
+                  console.log("Vocal No pertenece");
+                  intentosRestantes--;
             }
-            else if (incluyeLetra(diccionario[0],$(".letraEntrada").val())) {
-                  $("var").append($(".letraEntrada").val());
+            else if (incluyeLetra(palabraOculta,$(".letraEntrada").val())<=palabraOculta.length) {
+
+                  console.log(`.letra${incluyeLetra(palabraOculta,$(".letraEntrada").val())}`);
+                  $(`.letra${incluyeLetra(palabraOculta,$(".letraEntrada").val())}`).empty();
+                  $(`.letra${incluyeLetra(palabraOculta,$(".letraEntrada").val())}`).append($(".letraEntrada").val());
+                  $(`.letra${incluyeLetra(palabraOculta,$(".letraEntrada").val())}`).data("encontrado", true)
             }
-            
-            
+            //${incluyeLetra(palabraOculta,$(".letraEntrada").val())}
+            //${incluyeLetra(palabraOculta,$(".letraEntrada").val())}
             
       });
 });
